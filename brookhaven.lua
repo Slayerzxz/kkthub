57551a3adde032719983787f8438e8c6b006214a
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 15:01:51 -0300

    Update and rename README.md to brookhaven.lua

---
+++ brookhaven.lua
@@ -0,0 +1,18 @@
+-- This script as made by Bazuka
+--[[
+
+-----------------------------------------------------------------------------------------
+It is public only for functional use; misuse of the script may lead to legal action.
+
+está publico apenas para uso de funções, o uso indevido do script poderá gerar processos judiciais.
+
+-------------------------------------------------------------------------------------------
+You can't take the full source, change the name, and publish it; if you do, I will contact Scriptblox support asking for the script to be removed immediately.
+
+Você não pode pegar a source completa, mudar o nome e publicar, se fizer, irei entrar em contato com o suporte da scriptblox pedindo a remoção imediata do script.
+
+-------------------------------------------------------------------------------------------
+
+]]
+
+--
